b3380baf85b48aed94a2972485f33f8b0008f203
Matt Morrisey <mattydevicce@gmail.com>
2015-11-05 17:28:20 -0500

    MAde some projects pages

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,27 @@
+class ProjectsController < ApplicationController
+
+	def new
+		@project = Project.new
+	end
+
+	def index
+		@projects = Project.all
+	end
+
+	def create
+		@project = Project.new(project_params, user: current_user)
+
+		if @project.save
+			redirect_to project_path(@project.id)
+		else
+			redirect_to new_project_path
+		end
+	end
+
+	private
+
+		def project_params
+      params.require(:project).permit(:name, :description, :est_time, :cost)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users
-
+  resources :projects
   get '/login'  => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
