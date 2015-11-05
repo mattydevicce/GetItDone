87b66ec723a63ff188822922549d9fa7b41a006d
Matt Morrisey <mattydevicce@gmail.com>
2015-11-05 16:04:01 -0500

    Added a toolbox model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105203801) do
+ActiveRecord::Schema.define(version: 20151105205837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "toolboxes", force: :cascade do |t|
+    t.string   "item"
+    t.string   "cost"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "toolboxes", ["user_id"], name: "index_toolboxes_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 20151105203801) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "toolboxes", "users"
 end
