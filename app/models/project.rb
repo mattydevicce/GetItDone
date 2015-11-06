349b0f989e9ab1bf77e03d5ec0fff1f2b3654443
Matt Morrisey <mattydevicce@gmail.com>
2015-11-06 15:57:59 -0500

    dev ops

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,10 +2,5 @@ class Project < ActiveRecord::Base
   belongs_to :user
   has_many :steps
 
-  has_attached_file :project_pic, styles: {
-    thumb: '100x100>',
-    square: '1000x1000>',
-    medium: '1000x10000>'
-  }
   validates_attachment_content_type :project_pic, :content_type => /\Aimage\/.*\Z/
 end
