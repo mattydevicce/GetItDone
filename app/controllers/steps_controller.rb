8da6db58cdb2db9ffe105d5417afd00cb4a160a3
Matt Morrisey <mattydevicce@gmail.com>
2015-11-05 20:03:24 -0500

    SAved steps to data base, need to figure out how to pass the project id also

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class StepsController < ApplicationController
 
 	def new
@@ -5,7 +6,12 @@ class StepsController < ApplicationController
 	end
 
 	def create
-		
+		params.each do |key, value|
+			if key.to_i > 0
+				Step.create({description: value, rank: key.to_i})
+			end
+		end
+		redirect_to user_path(current_user.id)
 	end
 
 end
