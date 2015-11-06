4380acb37c16e86674adcc9e8054e9ad452da73c
Matt Morrisey <mattydevicce@gmail.com>
2015-11-06 17:05:07 -0500

    Final touches before presentation

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#destroy'
 
   post '/submit_steps' => 'steps#create'
+
+  root 'projects#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
